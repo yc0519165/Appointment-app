fc1d7172ffac626557703c91d85770385b25d56b
yc0519165 <yc0519165@gmail.com>
2025-01-08 10:59:22 +0530

    added docs image in header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import styles from "../styles/Navbar.module.css";
 import { useNavigate } from "react-router-dom";
 import { assets } from "../assets/prescripto_assets/assets/assets_frontend/assets";
-import doc1Image from "/assets_frontend/doc1.png";
+
+
 
 export const Navbar = () => {
   const navigate = useNavigate();
@@ -22,7 +23,11 @@ export const Navbar = () => {
 
           {token ? (
             <div className={`${styles.profile} group`}>
-              <img className={`${styles.profileImag}`} src={doc1Image} alt="" />
+              <img
+                className={`${styles.profileImag}`}
+                src={assets.profile_pic}
+                alt=""
+              />
               <img className="w-2.5" src={assets.user} alt="" />
               <div className="absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden transition-0.2s group-hover:block">
                 <div className="min-w-48 bg-slate-200 rounded flex flex-col gap-4 p-4">
