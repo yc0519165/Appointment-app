6afea8d8eb9b54718f759debd4a3ffa27ebff108
yc0519165 <yc0519165@gmail.com>
2025-01-08 11:36:15 +0530

    update pleae

--- src/assets/prescripto_assets/assets/assets_frontend/assets.js
+++ src/assets/prescripto_assets/assets/assets_frontend/assets.js
@@ -15,7 +15,7 @@ import info_icon from "./info_icon.svg";
 import upload_icon from "./upload_icon.png";
 import stripe_logo from "./stripe_logo.png";
 import razorpay_logo from "./razorpay_logo.png";
-import doc1 from "./doc1.png";
+import doc1 from "/src/assets/prescripto_assets/assets/assets_frontend/doc1.png";
 import doc2 from "./doc2.png";
 import doc3 from "./doc3.png";
 import doc4 from "./doc4.png";
